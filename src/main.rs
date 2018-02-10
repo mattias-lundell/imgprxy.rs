891e5fd433e867d7f0c5eebb89da4c1d7c11ae31
Mattias Lundell <mattias@fishbrain.com>
2018-02-10 20:30:57 +0100

    Move image manipulation into methods on Image struct.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 #![feature(plugin, custom_derive)]
 #![plugin(rocket_codegen)]
 
-extern crate rocket;
-extern crate reqwest;
-extern crate image;
 extern crate envy;
+extern crate image;
+extern crate reqwest;
+extern crate rocket;
 extern crate url;
 
 #[macro_use]
@@ -18,11 +18,11 @@ extern crate error_chain;
 
 use std::collections::HashSet;
 use std::io::copy;
-use std::io::{Cursor};
+use std::io::Cursor;
 use image::GenericImage;
 use rocket::request::FromFormValue;
 use rocket::http::RawStr;
-use url::{Url};
+use url::Url;
 
 error_chain! {
     foreign_links {
@@ -33,7 +33,7 @@ error_chain! {
 }
 
 struct Image {
-    data: image::DynamicImage
+    data: image::DynamicImage,
 }
 
 impl Image {
@@ -41,6 +41,45 @@ impl Image {
         let mut c = Cursor::new(Vec::new());
         self.data.save(&mut c, image::JPEG).unwrap();
         c
+    }
+
+    // scale image so that the whole image fits inside rectangle given by height and width.
+    fn fit(self, height: Option<u32>, width: Option<u32>) -> Image {
+        let old_h = height.unwrap_or(self.data.height());
+        let old_w = width.unwrap_or(self.data.width());
+
+        let thumbnail = self.data.resize(old_w, old_h, image::FilterType::Lanczos3);
+
+        Image { data: thumbnail }
+    }
+
+    // scale image so that it fills up whole rectangle given by height and width,
+    // then crop image to rectangle.
+    fn fill(self, height: Option<u32>, width: Option<u32>) -> Image {
+        let old_h = self.data.height() as f32;
+        let old_w = self.data.width() as f32;
+
+        let target_h = height.unwrap_or(self.data.height()) as f32;
+        let target_w = width.unwrap_or(self.data.width()) as f32;
+
+        let ratio_h = target_h / old_h;
+        let ratio_w = target_w / old_w;
+        let ratio = ratio_w.max(ratio_h);
+
+        let new_h = old_h * ratio;
+        let new_w = old_w * ratio;
+
+        let y_0 = (((new_h - target_h) as f32) / 2.0) as u32;
+        let x_0 = (((new_w - target_w) as f32) / 2.0) as u32;
+
+        let mut thumbnail =
+            self.data
+                .resize(new_w as u32, new_h as u32, image::FilterType::Lanczos3);
+        let cropped_thumbnail = thumbnail.crop(x_0, y_0, target_w as u32, target_h as u32);
+
+        Image {
+            data: cropped_thumbnail,
+        }
     }
 }
 
@@ -53,55 +92,24 @@ impl<'r> rocket::response::Responder<'r> for Image {
     }
 }
 
-// scale image so that the whole image fits inside rectangle given by height and width.
-fn fit(img: image::DynamicImage, height: Option<u32>, width: Option<u32>) -> Image {
-    let old_h = height.unwrap_or(img.height());
-    let old_w = width.unwrap_or(img.width());
-
-    let thumbnail = img.resize(old_w, old_h, image::FilterType::Lanczos3);
-
-    Image{data: thumbnail}
-}
-
-// scale image so that it fills up whole rectangle given by height and width,
-// then crop image to rectangle.
-fn fill(img: image::DynamicImage, height: Option<u32>, width: Option<u32>) -> Image {
-    let old_h = img.height() as f32;
-    let old_w = img.width() as f32;
-
-    let target_h = height.unwrap_or(img.height()) as f32;
-    let target_w = width.unwrap_or(img.width()) as f32;
-
-    let ratio_h = target_h / old_h;
-    let ratio_w = target_w / old_w;
-    let ratio = ratio_w.max(ratio_h);
-
-    let new_h = old_h * ratio;
-    let new_w = old_w * ratio;
-
-    let y_0 = (((new_h - target_h) as f32) / 2.0) as u32;
-    let x_0 = (((new_w - target_w) as f32) / 2.0) as u32;
-
-    let mut thumbnail = img.resize(new_w as u32, new_h as u32, image::FilterType::Lanczos3);
-    let cropped_thumbnail = thumbnail.crop(x_0, y_0, target_w as u32, target_h as u32);
-
-    Image{data: cropped_thumbnail}
-}
-
-fn resize_image(img: image::DynamicImage, mode: String, height: Option<u32>, width: Option<u32>) -> Result<Image> {
+fn resize_image(
+    img: Image,
+    mode: String,
+    height: Option<u32>,
+    width: Option<u32>,
+) -> Result<Image> {
     if mode == "fill" {
-        return Ok(fill(img, height, width));
+        return Ok(img.fill(height, width));
     } else if mode == "fit" {
-        return Ok(fit(img, height, width));
+        return Ok(img.fit(height, width));
     }
     unreachable!();
 }
 
-fn download_image(url: &str) -> Result<image::DynamicImage> {
+fn download_image(url: &str) -> Result<Image> {
     let mut res = reqwest::get(url)?;
     if res.status().is_success() {
-        let len = res
-            .headers()
+        let len = res.headers()
             .get::<reqwest::header::ContentLength>()
             .map(|ct_len| **ct_len)
             .unwrap_or(0);
@@ -111,7 +119,7 @@ fn download_image(url: &str) -> Result<image::DynamicImage> {
             let mut buf = Vec::with_capacity(len as usize);
             copy(&mut res, &mut buf)?;
             let img = image::load_from_memory(buf.as_slice())?;
-            Ok(img)
+            Ok(Image { data: img })
         }
     } else {
         Err("Request was not successful".into())
@@ -123,7 +131,7 @@ struct ResizeRequest {
     url: ValidUrl,
     mode: Option<String>,
     height: Option<u32>,
-    width: Option<u32>
+    width: Option<u32>,
 }
 
 #[derive(Debug)]
@@ -134,9 +142,7 @@ impl<'v> FromFormValue<'v> for ValidUrl {
 
     fn from_form_value(form_value: &'v RawStr) -> Result<ValidUrl> {
         match form_value.parse::<Url>() {
-            Ok(url) => {
-                Ok(ValidUrl(url))
-            },
+            Ok(url) => Ok(ValidUrl(url)),
             _ => Err("Invalid URL".into()),
         }
     }
@@ -144,7 +150,7 @@ impl<'v> FromFormValue<'v> for ValidUrl {
 
 #[derive(Deserialize, Debug)]
 struct Config {
-    url_whitelist: Vec<String>
+    url_whitelist: Vec<String>,
 }
 
 lazy_static! {
@@ -160,7 +166,7 @@ lazy_static! {
 fn valid_host(url: &Url) -> bool {
     match url.host_str() {
         Some(url) => URL_WHITELIST.contains(url),
-        None => false
+        None => false,
     }
 }
 
@@ -172,8 +178,8 @@ fn retrieve(request: ResizeRequest) -> Result<Image> {
             img,
             request.mode.unwrap_or("fit".to_string()),
             request.height,
-            request.width
-        )
+            request.width,
+        );
     }
     Err("Invalid hostname".into())
 }
